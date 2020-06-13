97a34ca4660ac2f27ae3d3479627bbc3f90a5b3b
Matt Rudge <mrudge@gmail.com>
2020-06-13 12:19:20 +0000

    feat: added error URL keyword

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def index():
     if response.status_code == 200:
         return redirect(SUCCESS_URL)
     else:
-        return redirect(f"{FAILURE_URL}?{response.status_code}")
+        return redirect(f"{FAILURE_URL}?code={response.status_code}")
 
 
 if __name__ == "__main__":
