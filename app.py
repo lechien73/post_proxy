0e508b9574dda157e67bf1b59bfd3f75de3f1a60
Matt Rudge <mrudge@gmail.com>
2020-06-13 12:00:32 +0000

    feat: add form fields

--- app.py
+++ app.py
@@ -8,13 +8,22 @@ import requests
 
 
 app = Flask(__name__)
-WEBHOOK = os.environ.get("ZAPIER_WEBHOOK")
-SUCCESS_URL = os.environ.get("SUCCESS_URL")
-FAILURE_URL = os.environ.get("FAILURE_URL")
 
 
 @app.route("/", methods=["POST"])
 def index():
+    """
+    Main controller, checks for x_webhook, x_success and x_failure
+    form fields. Uses environment variables if they're not available.
+    """
+
+    WEBHOOK = request.form["x_webhook"] if request.form["x_webhook"] else os.environ.get(
+        "ZAPIER_WEBHOOK", "")
+    SUCCESS_URL = request.form["x_success"] if request.form["x_success"] else os.environ.get(
+        "SUCCESS_URL", "")
+    FAILURE_URL = request.form["x_failure"] if request.form["x_failure"] else os.environ.get(
+        "FAILURE_URL", "")
+
     response = requests.post(WEBHOOK, data=request.form)
 
     if response.status_code == 200:
