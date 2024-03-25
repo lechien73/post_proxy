de395274b461ae7104b16d2503e033b11bef54f4
Matt Rudge <matt.rudge@codeinstitute.net>
2024-03-25 15:12:38 +0000

    Add success response

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ def index():
             data = [item + "=" + request.form[item] for item in x_data]
 
         success = SUCCESS_URL if SUCCESS_URL[0:3] == "http" else request.referrer + SUCCESS_URL
+        print(url)
         return redirect(f"{success}?{'&'.join(data) if data else ''}")
     else:
         return redirect(f"{FAILURE_URL}?code={response.status_code}")
